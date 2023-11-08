1414591842f3d2bbe5aaabd1f7314b4a4ad3b490
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-11-08 14:12:47 +0100

    Start day 10 implementation

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,7 +1,21 @@
 use advent_of_code::Solution;
 
+const SIZE: usize = 5;
+
 pub fn part_one(input: &str) -> Solution {
-    println!("{}", input);
+    let mut elements: [usize; SIZE] = [0; SIZE];
+
+    for (index, value) in elements.iter_mut().enumerate() {
+        *value = index;
+    }
+
+    let mut pos: usize = 0;
+    let mut skip_size: usize = 0;
+    for v in input.split(",") {
+        skip_size += 1;
+    }
+
+    println!("{:?}", elements);
     Solution::Empty
 }
 
@@ -10,7 +24,7 @@ pub fn part_two(input: &str) -> Solution {
 }
 
 fn main() {
-    let input = &advent_of_code::read_file("inputs", 10);
+    let input = &advent_of_code::read_file("examples", 10);
     advent_of_code::solve!(1, part_one, input);
     advent_of_code::solve!(2, part_two, input);
 }
