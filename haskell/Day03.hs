93e315cb0dd286110a94153e8fe063438c460d08
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-01-25 17:33:59 +0100

    solve 2017 day 3

--- haskell/Day03.hs
+++ haskell/Day03.hs
@@ -1,6 +1,13 @@
-input = 289326
+import Data.Maybe (mapMaybe)
+import qualified Data.Map as M
+
+neighbours = [-1, 0, 1]
+
+type Coord = (Int, Int)
+data Dir = R | U | L | D deriving (Enum)
 
 main = do
+    input <- read <$> readFile "inputs/3.txt"
     print $ part1 input
     print $ part2 input
 
@@ -12,10 +19,39 @@ part1 num =
     in length rings + minimum (map (\p -> abs (num - p)) ringAxes)
 
 part2 :: Int -> Int
-part2 num = 0
+part2 num = go generateCoordinates $ M.singleton (0, 0) 1
+    where
+        go :: [Coord] -> M.Map Coord Int -> Int
+        go (c:cs) grid
+            | neighbourSums > num = neighbourSums
+            | otherwise = go cs (M.insert c neighbourSums grid)
+            where neighbourSums = sum $ mapMaybe (`M.lookup` grid) $ getNeighbours c
 
 generateAxes :: [Int]
 generateAxes = scanl (+) 1 $ gen 1
     where
         gen :: Int -> [Int]
         gen num = num : replicate 3 (num + 1) ++ gen (num + 2)
+
+generateCoordinates :: [Coord]
+generateCoordinates = gen (generateMoves 1) (0, 0) R
+    where
+        gen :: [Int] -> Coord -> Dir -> [Coord]
+        gen (n:ns) c dir = coords ++ gen ns (last coords) (nextDir dir)
+            where coords = take n $ iterate (moveCoordinate dir) $ moveCoordinate dir c
+
+moveCoordinate :: Dir -> Coord -> Coord
+moveCoordinate R (x, y) = (x + 1, y)
+moveCoordinate U (x, y) = (x, y + 1)
+moveCoordinate L (x, y) = (x - 1, y)
+moveCoordinate D (x, y) = (x, y - 1)
+
+generateMoves :: Int -> [Int]
+generateMoves n = n : n : generateMoves (n + 1)
+
+nextDir :: Dir -> Dir
+nextDir D = R
+nextDir d = succ d
+
+getNeighbours :: Coord -> [Coord]
+getNeighbours (x, y) = [(x + i, y + j) | i <- neighbours, j <- neighbours, (i, j) /= (0, 0)]
